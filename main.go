37531ee8b04ef203892d562ec900cf9eda8bb5c2
devopscodeck <you@example.com>
2022-05-28 18:41:45 -0300

    First commit, add gin framework

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func ExibeTodosAlunos(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"id":   "1",
+		"nome": "Galileu",
+	})
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/alunos", ExibeTodosAlunos)
+	r.Run(":5000")
+}
